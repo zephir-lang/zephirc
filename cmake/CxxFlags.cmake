e6dd1c5214b26fab8262292dab71cfbbda62813a
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-17 23:50:31 +0300

    Setup CXX flags

---
+++ cmake/CxxFlags.cmake
@@ -0,0 +1,53 @@
+# RELEASE Flags
+
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wall -O3")
+
+# DEBUG Flags
+
+# Forcing the compiler to work harder helps finding potential problems
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O")
+
+# Show all warnings
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+
+# Enable some extra warning flags
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wextra")
+
+# Warn if anything depends upn the size of a function or of void
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wpointer-arith")
+
+# Avoid int i = i
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Winit-self")
+
+# Dont check floats for equality
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wfloat-equal")
+
+# Warn if an unitialized identifier is evaluated
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wundef")
+
+# Warn whenever a local variable shadows another local/global one
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wshadow")
+
+# Warn if a pointer is cast such that alignment of target is increased
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wcast-align")
+
+# Give string consts the type const char[length}
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wwrite-strings")
+
+# Warn whenever a poinster is cast to remove a type qualifier from target
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wcast-qual")
+
+# Warn whenever a switch does not have a default case
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wswitch-default")
+
+# Warn whenever a switch has an index of enumerated type and lacks a case
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wswitch-enum")
+
+# Warn for implicit conversions that may alter a value
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wconversion")
+
+# Warn ifh the compiler detects code that will never be executed
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wunreachable-code")
+
+# Issue all warnings demanded by strict ISO C++
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -pedantic")
