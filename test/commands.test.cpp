a53cc66d868a34f587be34eb4fc679cb12100baa
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-19 00:02:18 +0300

    Cleaned up test

--- test/commands.test.cpp
+++ test/commands.test.cpp
@@ -1,24 +1,7 @@
 #include <catch2/catch.hpp>
-#include <cstdarg>
 
+#include "test_helpers.hpp"
 #include "../src/commands/cmd.hpp"
-
-char **makeArgv(int count, ...)
-{
-    va_list args;
-    int i;
-    char **argv = (char **)malloc((static_cast<unsigned long>(count + 1)) * sizeof(char*));
-    char *temp;
-    va_start(args, count);
-    for (i = 0; i < count; i++) {
-        temp = va_arg(args, char*);
-        argv[i] = (char*)malloc(sizeof(temp+1));
-        strcpy(argv[i],temp);
-    }
-    argv[i] = nullptr;
-    va_end(args);
-    return argv;
-}
 
 SCENARIO("Compiler can be invoked without arguments", "[commands]") {
     GIVEN("an empty argument list") {
