6dba13a6cac8b19b35625324c1e12d86b3aa7784
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-19 02:02:41 +0300

    Update entrypoint

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,19 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+
 #include <catch2/catch.hpp>
+
+/*
+ * This entrypoint should be used to provide a custom
+ * code that executes before and/ or after Catch
+ *
+ * See: https://github.com/catchorg/Catch2/blob/master/docs/own-main.md#top
+ */
+int main(int argc, char *argv[]) {
+    // global setup...
+
+    int result = Catch::Session().run(argc, argv);
+
+    // global clean-up...
+
+    return result;
+}
