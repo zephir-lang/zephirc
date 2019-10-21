6d607a1331ddbb6a1bdeac03fd1e70d61a47242c
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-10-22 00:25:06 +0300

    Added tests

--- test/commands.test.cpp
+++ test/commands.test.cpp
@@ -8,19 +8,20 @@ class CommandsTest : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(CommandsTest);
 
-    CPPUNIT_TEST(CommandParse);
-
+    CPPUNIT_TEST(Compiler_can_be_invoked_without_arguments);
+    CPPUNIT_TEST(command_is_API);
     CPPUNIT_TEST_SUITE_END();
 
 public:
-    void testExample();
-    void CommandParse();
+    void Compiler_can_be_invoked_without_arguments();
+    void command_is_API();
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( CommandsTest );
 
 
-void CommandsTest::CommandParse()
+void CommandsTest::Compiler_can_be_invoked_without_arguments()
 {
     Argv argv({
             "zephir",
@@ -35,4 +36,32 @@ void CommandsTest::CommandParse()
     auto result = commands::parseopt(args, cmd);
 
     CPPUNIT_ASSERT(cmd.kind == commands::NONE);
+    CPPUNIT_ASSERT(cmd.quiet == false);
+    CPPUNIT_ASSERT(cmd.common_help == false);
+    CPPUNIT_ASSERT(cmd.version == false);
+    CPPUNIT_ASSERT(cmd.vernum == false);
+    CPPUNIT_ASSERT(cmd.dumpversion == false);
+}
+
+void CommandsTest::command_is_API()
+{
+    Argv argv({
+            "zephir",
+            "api",
+            ""
+        });
+
+    char **args = argv.argv();
+
+    commands::Cmd cmd;
+    std::memset(&cmd, 0, sizeof(cmd));
+
+    auto result = commands::parseopt(args, cmd);
+
+    CPPUNIT_ASSERT(cmd.kind == commands::API);
+    CPPUNIT_ASSERT(cmd.quiet == false);
+    CPPUNIT_ASSERT(cmd.common_help == false);
+    CPPUNIT_ASSERT(cmd.version == false);
+    CPPUNIT_ASSERT(cmd.vernum == false);
+    CPPUNIT_ASSERT(cmd.dumpversion == false);
 }
