e58757c2c08182cf80922480a51895de58fd12a1
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-21 21:48:35 +0300

    Improved tests

--- test/commands.test.cpp
+++ test/commands.test.cpp
@@ -16,7 +16,7 @@ Test(commands, empty)
     std::memset(&cmd, 0, sizeof(cmd));
 
     // command is NONE
-    cr_assert_eq(commands::parseopt(args, cmd), true);
+    cr_expect(commands::parseopt(args, cmd));
     cr_assert_eq(cmd.kind, commands::NONE);
 
     // all global options in default state
@@ -41,21 +41,87 @@ Test(commands, api_empty)
     std::memset(&cmd, 0, sizeof(cmd));
 
     // command is NONE
-    cr_assert_eq(commands::parseopt(args, cmd), true);
+    cr_expect(commands::parseopt(args, cmd));
     cr_assert_eq(cmd.kind, commands::API);
 
     // all API options are in default state
-    cr_assert_eq(cmd.api.backend, 0);
-    cr_assert_eq(cmd.api.path, 0);
-    cr_assert_eq(cmd.api.output, 0);
-    cr_assert_eq(cmd.api.options, 0);
-    cr_assert_eq(cmd.api.url, 0);
-    cr_assert_eq(cmd.api.help, 0);
+    cr_assert_not(cmd.api.backend);
+    cr_assert_not(cmd.api.path);
+    cr_assert_not(cmd.api.output);
+    cr_assert_not(cmd.api.options);
+    cr_assert_not(cmd.api.url);
+    cr_assert_not(cmd.api.help);
 
     // all global options are in default state
-    cr_assert_eq(cmd.quiet, 0);
-    cr_assert_eq(cmd.common_help, 0);
-    cr_assert_eq(cmd.version, 0);
-    cr_assert_eq(cmd.vernum, 0);
-    cr_assert_eq(cmd.dumpversion, 0);
+    cr_assert_not(cmd.quiet);
+    cr_assert_not(cmd.common_help);
+    cr_assert_not(cmd.version);
+    cr_assert_not(cmd.vernum);
+    cr_assert_not(cmd.dumpversion);
+}
+
+Test(commands, api_help)
+{
+    Argv argv({
+        "zephir",
+        "api",
+        "--help",
+        ""
+    });
+
+    char **args = argv.argv();
+
+    commands::Cmd cmd;
+    std::memset(&cmd, 0, sizeof(cmd));
+
+    // command is NONE
+    cr_expect(commands::parseopt(args, cmd));
+    cr_assert_eq(cmd.kind, commands::API);
+
+    // only help option was changed
+    cr_assert_not(cmd.api.backend);
+    cr_assert_not(cmd.api.path);
+    cr_assert_not(cmd.api.output);
+    cr_assert_not(cmd.api.options);
+    cr_assert_not(cmd.api.url);
+    cr_expect(cmd.api.help);
+
+    // all global options are in default state
+    cr_assert_not(cmd.quiet);
+    cr_assert_not(cmd.common_help);
+    cr_assert_not(cmd.version);
+    cr_assert_not(cmd.vernum);
+    cr_assert_not(cmd.dumpversion);
+}
+
+Test(commands, api_full)
+{
+    Argv argv({
+        "zephir",
+        "api",
+        "--url=http://test.com",
+        "--backend=ZendEngine3",
+        "-p", "theme",
+        "-o", "out",
+        "--options=opts",
+        ""
+    });
+
+    char **args = argv.argv();
+
+    commands::Cmd cmd;
+    std::memset(&cmd, 0, sizeof(cmd));
+
+    // command is NONE
+    cr_expect(commands::parseopt(args, cmd));
+    cr_assert_eq(cmd.kind, commands::API);
+
+    // only help option was changed
+    cr_assert_str_eq( cmd.api.backend, "ZendEngine3");
+    cr_assert_str_eq(cmd.api.path, "theme");
+    cr_assert_str_eq(cmd.api.output, "out");
+    cr_assert_str_eq(cmd.api.options, "opts");
+    cr_assert_str_eq(cmd.api.url, "http://test.com");
+    cr_assert_not(cmd.api.help);
+
 }
