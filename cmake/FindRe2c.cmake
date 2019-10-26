b60ed3e546f1244f3ff2bae5cec9be98a018017e
Serghei Iakovlev <sadhooklay@gmail.com>
2019-10-26 12:13:36 +0300

    Cleaned up FindRe2c.cmake module

--- cmake/FindRe2c.cmake
+++ cmake/FindRe2c.cmake
@@ -45,8 +45,6 @@ elseif(WIN32)
                DOC "path to the re2c executable")
 endif()
 
-set(RE2C_VERSION "")
-
 mark_as_advanced(RE2C_EXECUTABLE)
 
 if(RE2C_EXECUTABLE)
@@ -56,52 +54,39 @@ if(RE2C_EXECUTABLE)
                   ERROR_VARIABLE RE2C_VERSION_ERROR
                   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  string(REGEX
-         REPLACE "^re2c "
-                 ""
-                 RE2C_VERSION_OUTPUT
-                 "${RE2C_VERSION_OUTPUT}")
+  string(REGEX REPLACE "^re2c " "" RE2C_VERSION_OUTPUT "${RE2C_VERSION_OUTPUT}")
 
   if(RE2C_VERSION_RESULT EQUAL 0)
     set(RE2C_VERSION ${RE2C_VERSION_OUTPUT})
   else()
-    message(
-      SEND_ERROR
-        "Command \"${RE2C_EXECUTABLE} --version\" failed with output:\n${RE2C_VERSION_ERROR}"
-      )
+    message(SEND_ERROR
+      "Command \"${RE2C_EXECUTABLE} --version\" failed with output:\n${RE2C_VERSION_ERROR}"
+    )
   endif()
 
   # RE2C_TARGET (public macro) TODO - rework this macro to make use of
   # CMakeParseArguments, see
   # http://www.cmake.org/pipermail/cmake/2012-July/051309.html
   if(NOT COMMAND RE2C_TARGET)
-    macro(RE2C_TARGET
-          Name
-          Input
-          Output)
-      set(RE2C_TARGET_USAGE
-          "RE2C_TARGET(<Name> <Input> <Output> [COMPILE_FLAGS <string>]")
+    macro(RE2C_TARGET Name Input Output)
       if(${ARGC} GREATER 3)
         if(${ARGC} EQUAL 5)
           if("${ARGV3}" STREQUAL "COMPILE_FLAGS")
             set(RE2C_EXECUTABLE_OPTS "${ARGV4}")
             separate_arguments(RE2C_EXECUTABLE_OPTS)
           else()
-            message(SEND_ERROR ${RE2C_TARGET_USAGE})
+            message(SEND_ERROR "Invalid RE2C_TARGET usage: Incorrect ARGV3")
           endif()
         else()
-          message(SEND_ERROR ${RE2C_TARGET_USAGE})
+          message(SEND_ERROR "Invalid RE2C_TARGET usage: Incorrect ARGC value")
         endif()
       endif()
 
       add_custom_command(
-        OUTPUT ${Output}
-        COMMAND ${RE2C_EXECUTABLE}
-                ARGS
-                ${RE2C_EXECUTABLE_OPTS}
-                -o${Output}
-                ${Input}
+        OUTPUT  ${Output}
         DEPENDS ${Input} ${RE2C_EXECUTABLE_TARGET}
+        COMMAND ${RE2C_EXECUTABLE}
+        ARGS    ${RE2C_EXECUTABLE_OPTS} -o${Output} ${Input}
         COMMENT
           "[RE2C][${Name}] Building scanner with ${RE2C_EXECUTABLE} v${RE2C_VERSION}"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
@@ -111,8 +96,7 @@ if(RE2C_EXECUTABLE)
       set(RE2C_${Name}_INPUT ${Input})
       set(RE2C_${Name}_COMPILE_FLAGS ${RE2C_EXECUTABLE_OPTS})
 
-      set_property(DIRECTORY
-                   APPEND
+      set_property(DIRECTORY APPEND
                    PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "${Output}")
     endmacro()
   endif()
@@ -123,8 +107,6 @@ find_package_handle_standard_args(
   RE2C
   "No re2c found in the $PATH. Consider install re2c or/and add re2c executable to the $PATH"
   RE2C_EXECUTABLE)
-
-mark_as_advanced(RE2C_VERSION)
 
 # FindRe2c.cmake ends here
 
