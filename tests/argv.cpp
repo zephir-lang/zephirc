94704a2a04f5628415a1fc174130bdbde6f30419
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-03 18:53:37 +0200

    Replace strcpy by more safer strncpy

--- tests/argv.cpp
+++ tests/argv.cpp
@@ -2,13 +2,14 @@
 //
 // (c) Zephir Team <team@zephir-lang.com>
 //
-// For the full copyright and license information, please view the LICENSE
-// file that was distributed with this source code.
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
 
 #include "argv.hpp"
 
 #include <cstring>
 #include <initializer_list>
+#include <utility>
 
 Argv::Argv(std::initializer_list<const char *> args)
     : m_argv(new char *[args.size()]), m_argc(args.size()) {
@@ -18,7 +19,7 @@ Argv::Argv(std::initializer_list<const char *> args)
     auto len = std::strlen(*iter) + 1;
     auto ptr = std::unique_ptr<char[]>(new char[len]);
 
-    std::strcpy(ptr.get(), *iter);
+    std::strncpy(ptr.get(), *iter, len);
     m_args.push_back(std::move(ptr));
     m_argv.get()[i] = m_args.back().get();
 
