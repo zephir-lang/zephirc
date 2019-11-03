5e9b349267e5d8c0025efd3b0383287c68b732a0
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-03 18:51:35 +0200

    Correct code style

--- src/version.hpp
+++ src/version.hpp
@@ -2,8 +2,8 @@
 //
 // (c) Zephir Team <team@zephir-lang.com>
 //
-// For the full copyright and license information, please view the LICENSE
-// file that was distributed with this source code.
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
 
 #ifndef ZEPHIR_VERSION_HPP_
 #define ZEPHIR_VERSION_HPP_
@@ -12,8 +12,8 @@
 #define ZEPHIR_VERSION_MINOR 0
 #define ZEPHIR_VERSION_PATCH 0
 
-#define ZEPHIR_VERSION_ID \
-	(((ZEPHIR_VERSION_MAJOR)*10000000) + ((ZEPHIR_VERSION_MINOR)*100000) \
-	 + (ZEPHIR_VERSION_PATCH))
+#define ZEPHIR_VERSION_ID                                                \
+  (((ZEPHIR_VERSION_MAJOR)*10000000) + ((ZEPHIR_VERSION_MINOR)*100000) + \
+  (ZEPHIR_VERSION_PATCH))
 
-#endif // ZEPHIR_VERSION_HPP_
+#endif  // ZEPHIR_VERSION_HPP_
