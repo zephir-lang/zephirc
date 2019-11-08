6f8c199f27203fe639554536059f4a48697956ac
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-09 00:46:14 +0200

    Move to CLI11

--- cmake/build-types.cmake
+++ cmake/build-types.cmake
@@ -11,3 +11,14 @@ if (NOT CMAKE_BUILD_TYPE)
 elseif(NOT CMAKE_BUILD_TYPE IN_LIST allowableBuildTypes)
     message(FATAL_ERROR "Invalid build type: ${CMAKE_BUILD_TYPE}")
 endif()
+
+# Make sure developers do not run cmake in the main project directory, to keep
+# build artifacts from becoming clutter
+if (${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+    string(CONCAT IN_BUILD_ERROR
+            "In-source builds not allowed. Please make a new directory "
+            "(called a build directory) and run CMake from there. "
+            "You may need to remove CMakeCache.txt.")
+
+    message(FATAL_ERROR "${IN_BUILD_ERROR}")
+endif()
