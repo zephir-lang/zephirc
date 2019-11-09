cf3019e5f652a52e98d9209fb2a74a60e7a00078
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-09 11:20:53 +0200

    Remove unused part from formatter

--- src/commands/formatter.cpp
+++ src/commands/formatter.cpp
@@ -32,21 +32,6 @@ std::string commands::Formatter::make_usage(const CLI::App *app,
     out << " [" << get_label("options") << "]";
   }
 
-  // Positionals need to be listed here
-  std::vector<const CLI::Option *> positionals = app->get_options(
-      [](const CLI::Option *opt) { return opt->get_positional(); });
-
-  // Print out positionals if any are left
-  if (!positionals.empty()) {
-    // Convert to help names
-    std::vector<std::string> positional_names(positionals.size());
-    std::transform(
-        positionals.begin(), positionals.end(), positional_names.begin(),
-        [this](const CLI::Option *opt) { return make_option_usage(opt); });
-
-    out << " " << CLI::detail::join(positional_names, " ");
-  }
-
   out << " [arguments]";
   out << std::endl;
 
