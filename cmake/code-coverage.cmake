21c1c0dbdc84be378239387583e70cebe61895bb
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-09 11:46:54 +0200

    Change email

--- cmake/code-coverage.cmake
+++ cmake/code-coverage.cmake
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018 by George Cave - gcave@stablecoder.ca
-# Copyright (C) 2019 by Sergei Iakovlev - serghei@phalcon.io
+# Copyright (C) 2019 by Sergei Iakovlev - egrep@protonmail.ch
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
