7b1844e503854f4378d4ec9aa2d8b1a61349916d
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-09 20:27:56 +0200

    Code cleanup

--- src/config.cpp
+++ src/config.cpp
@@ -1,7 +1,9 @@
 #include "zephir/config.hpp"
+
 #include "commands/commands.hpp"
 
-int core::load_config(core::Config *config, int argc, char **argv, std::string config_file) {
+int core::load_config(core::Config *config, int argc, char **argv,
+                      std::string config_file) {
   auto retval = commands::optparse(argc, argv);
   if (retval == ZEPHIR_COMMANDS_EXIT_HELP) {
     retval = 0;
