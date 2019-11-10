97e75c6816c620f84729b2eb01151bbbf2ae47b5
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-10 18:50:33 +0200

    Added stub to parse config

--- src/config.cpp
+++ src/config.cpp
@@ -7,13 +7,25 @@
 
 #include "zephir/config.hpp"
 
+#include <yaml-cpp/yaml.h>
+
 #include "commands/commands.hpp"
+
+namespace {
+void parse_yaml_config(core::Config *config, const std::string &config_file) {
+  // TODO(klay): Implement me.
+}
+}  // namespace
 
 int core::load_config(core::Config *config, int argc, char **argv,
                       std::string config_file) {
   auto retval = commands::optparse(argc, argv);
   if (retval == ZEPHIR_COMMANDS_EXIT_HELP) {
     retval = 0;
+  }
+
+  if (!config_file.empty()) {
+    parse_yaml_config(config, config_file);
   }
 
   return retval;
