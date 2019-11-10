e95887f19ba6ca19640bad69e695f7da0729da0e
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-10 11:40:50 +0200

    Minor refactor + add missed headers

--- src/config.cpp
+++ src/config.cpp
@@ -1,6 +1,12 @@
-#include "zephir/config.hpp"
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
 
 #include "commands/commands.hpp"
+#include "zephir/config.hpp"
 
 int core::load_config(core::Config *config, int argc, char **argv,
                       std::string config_file) {
