945242b6c41262e3ba84ecfdb2d530f252660d73
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-10 21:22:49 +0200

    Try to read file

--- src/config.cpp
+++ src/config.cpp
@@ -5,27 +5,51 @@
 // For the full copyright and license information, please view
 // the LICENSE file that was distributed with this source code.
 
-#include "zephir/config.hpp"
-
 #include <yaml-cpp/yaml.h>
+#include <filesystem>
 
+#include "zephir/main.hpp"
+#include "zephir/config.hpp"
 #include "commands/commands.hpp"
 
 namespace {
-void parse_yaml_config(core::Config *config, const std::string &config_file) {
+int parse_yaml_config(core::Config *config, const std::string &config_file) {
+  if (!std::filesystem::exists(config_file)) {
+    // Do nothing.
+    return EXIT_NO_CONFIG;
+  }
+
+  try {
+    YAML::Node loaded_config = YAML::LoadFile(config_file);
+  } catch (YAML::BadFile &e) {
+    // Do nothing.
+    return EXIT_NO_CONFIG;
+  } catch (YAML::ParserException &e) {
+    // TODO(klay): Probable we need to notify user about broken config file
+    return EXIT_BAD_CONFIG;
+  }
+
   // TODO(klay): Implement me.
+  return 0;
 }
 }  // namespace
 
 int core::load_config(core::Config *config, int argc, char **argv,
                       std::string config_file) {
   auto retval = commands::optparse(argc, argv);
-  if (retval == ZEPHIR_COMMANDS_EXIT_HELP) {
+  if (retval == EXIT_HELP) {
     retval = 0;
   }
 
+  if (retval != 0) {
+    return retval;
+  }
+
   if (!config_file.empty()) {
-    parse_yaml_config(config, config_file);
+    retval = parse_yaml_config(config, config_file);
+    if (retval == EXIT_NO_CONFIG) {
+      retval = 0;
+    }
   }
 
   return retval;
