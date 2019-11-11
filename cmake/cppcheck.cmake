50ab95ee88f9697f4272839316aef748eefd6dab
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-11 20:48:31 +0200

    Reformat cppcheck cmake module

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,28 +1,38 @@
-# If cppcheck is installed, create a target
-# to run it on the project source files.
+# This file is part of the Zephir.
+#
+# (c) Zephir Team <team@zephir-lang.com>
+#
+# For the full copyright and license information, please view the LICENSE file
+# that was distributed with this source code.
+
+# TODO(klay): Refacor me.
+
+# If cppcheck is installed, create a target to run it on the project source
+# files.
 if(UNIX)
-  find_program(CPPCHECK_EXECUTABLE
-               NAMES cppcheck
-               PATHS $ENV{CPPCHECK_DIR}
-                     /usr
-                     /usr/local
-                     /opt
-                     /opt/local
-               PATH_SUFFIXES bin
-               DOC "path to the cppcheck executable")
+    find_program(CPPCHECK_EXECUTABLE
+                 NAMES cppcheck
+                 PATHS $ENV{CPPCHECK_DIR}
+                       /usr
+                       /usr/local
+                       /opt
+                       /opt/local
+                 PATH_SUFFIXES bin
+                 DOC "path to the cppcheck executable")
 elseif(WIN32)
-  find_program(CPPCHECK_EXECUTABLE
-               NAMES cppcheck.exe
-               PATHS $ENV{CPPCHECK_DIR} C:/
-               PATH_SUFFIXES ""
-               DOC "path to the cppcheck executable")
+    find_program(CPPCHECK_EXECUTABLE
+                 NAMES cppcheck.exe
+                 PATHS $ENV{CPPCHECK_DIR} C:/
+                 PATH_SUFFIXES ""
+                 DOC "path to the cppcheck executable")
 endif()
 
 mark_as_advanced(CPPCHECK_EXECUTABLE)
 
 if(CPPCHECK_EXECUTABLE)
     message(STATUS "Check for cppcheck: ${CPPCHECK_EXECUTABLE}")
-    add_custom_target(cppcheck
+    add_custom_target(
+        cppcheck
         COMMAND "${CPPCHECK_EXECUTABLE}"
                 --quiet
                 --error-exitcode=1
@@ -37,8 +47,10 @@ endif()
 
 # cppcheck.cmake ends here
 
+# cmake-format: off
 # Local Variables:
 # mode: cmake
 # tab-width: 4
 # indent-tabs-mode: nil
 # End:
+# cmake-format: on
