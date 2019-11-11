05e3628031e7dba4f5d5df208fa95f7a9c5ab206
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-11 19:36:44 +0200

    Reformat version cmake module

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,18 +1,30 @@
+# This file is part of the Zephir.
+#
+# (c) Zephir Team <team@zephir-lang.com>
+#
+# For the full copyright and license information, please view the LICENSE file
+# that was distributed with this source code.
+
 file(STRINGS "${CMAKE_SOURCE_DIR}/include/zephir/version.hpp" zephir_version
-        REGEX "#define ZEPHIR_VERSION_(MAJOR|MINOR|PATCH)")
+     REGEX "#define ZEPHIR_VERSION_(MAJOR|MINOR|PATCH)")
 
 foreach(part ${zephir_version})
     if(part MATCHES "#define ZEPHIR_VERSION_(MAJOR|MINOR|PATCH) +([^ ]+)$")
-        set(ZEPHIR_VERSION_${CMAKE_MATCH_1} "${CMAKE_MATCH_2}" CACHE INTERNAL "")
+        set(ZEPHIR_VERSION_${CMAKE_MATCH_1}
+            "${CMAKE_MATCH_2}"
+            CACHE INTERNAL "")
     endif()
 endforeach()
 
-set(VERSION ${ZEPHIR_VERSION_MAJOR}.${ZEPHIR_VERSION_MINOR}.${ZEPHIR_VERSION_PATCH})
+set(VERSION
+    ${ZEPHIR_VERSION_MAJOR}.${ZEPHIR_VERSION_MINOR}.${ZEPHIR_VERSION_PATCH})
 
 # version.cmake ends here
 
+# cmake-format: off
 # Local Variables:
 # mode: cmake
 # tab-width: 4
 # indent-tabs-mode: nil
 # End:
+# cmake-format: on
