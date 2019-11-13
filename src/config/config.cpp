63a0c205a4e253832e9b4f54e193eec8ce0aecf2
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-13 23:03:16 +0200

    Minor refactor confilg structure

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -35,12 +35,21 @@ int parse_yaml_config(zephir::Config *config, const std::string &config_file) {
 }
 }  // namespace
 
+zephir::Config::Config() {
+  container.api.theme.options["github"] = "";
+  container.api.theme.options["analytics"] = "";
+  container.api.theme.options["main_color"] = "#3E6496";
+  container.api.theme.options["link_color"] = "#3E6496";
+  container.api.theme.options["link_hover_color"] = "#5F9AE7";
+}
+
 zephir::Config zephir::load_config(int argc, char **argv,
-                                   std::string config_file) {
+                                   const std::string &config_file) {
   zephir::Config config;
   auto retval = zephir::commands::optparse(argc, argv);
   if (retval == EXIT_HELP) {
-    retval = 0;
+    // Do nothing on "zephir --help" command.
+    return config;
   }
 
   if (retval != 0) {
@@ -49,8 +58,14 @@ zephir::Config zephir::load_config(int argc, char **argv,
 
   if (!config_file.empty()) {
     retval = parse_yaml_config(&config, config_file);
-    if (retval != EXIT_NO_CONFIG) {
-      // TODO(klay): Throw exception. Config is broken?
+    switch (retval) {
+      case 0:
+      // Nothing to do if we unable to find config file at the disk.
+      case EXIT_NO_CONFIG:
+        break;
+      default:
+        // TODO(klay): Throw exception. Config is broken?
+        break;
     }
   }
 
