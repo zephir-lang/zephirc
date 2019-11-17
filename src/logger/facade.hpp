d7553303c2c3aba5390cc6de58751c1ae3eb4bc0
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-18 00:09:17 +0200

    Add initial logger facade

---
+++ src/logger/facade.hpp
@@ -0,0 +1,17 @@
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+#ifndef ZEPHIR_LOGGER_FACADE_HPP_
+#define ZEPHIR_LOGGER_FACADE_HPP_
+
+#include <string>
+
+namespace zephir::logger {
+
+}  // namespace zephir::logger
+
+#endif  // ZEPHIR_LOGGER_FACADE_HPP_
