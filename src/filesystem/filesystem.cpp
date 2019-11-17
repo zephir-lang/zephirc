498127ce13ff43c27c57e16821a8c1f89aa0e783
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-17 23:22:53 +0200

    Cleanup filesystem wrapper

--- src/filesystem/filesystem.cpp
+++ src/filesystem/filesystem.cpp
@@ -10,7 +10,6 @@
 #include <unistd.h>
 
 #include <climits>
-#include <sstream>
 
 bool zephir::filesystem::exists(const std::string &name) {
   return (access(name.c_str(), F_OK) != -1);
@@ -18,25 +17,5 @@ bool zephir::filesystem::exists(const std::string &name) {
 
 std::string zephir::filesystem::current_path() {
   char temp[PATH_MAX];
-
-  if (getcwd(temp, PATH_MAX) != nullptr) {
-    return std::string(temp);
-  }
-
-  int error = errno;
-  std::string err_prefix = "An error occurred related to";
-  std::string err_suffix = "when obtaining current working directory";
-
-  switch (error) {
-    case EACCES:
-      throw std::runtime_error(err_prefix + " access rights " + err_suffix);
-    case ENOMEM:
-      throw std::runtime_error(err_prefix + " insufficient storage " +
-                               err_suffix);
-    default: {
-      std::ostringstream str;
-      str << "Unrecognised error occurred " << err_suffix << ": " << error;
-      throw std::runtime_error(str.str());
-    }
-  }
+  return (getcwd(temp, sizeof(temp)) ? std::string(temp) : std::string(""));
 }
