0344d241f24288f25b5ba435bf5bc81767c74636
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-19 12:35:39 +0200

    Add planned flow design

--- src/main.cpp
+++ src/main.cpp
@@ -14,5 +14,18 @@ int main(int argc, char** argv) {
 
   // TODO(klay): Catch exceptions
 
+  // TODO: Planned flow design
+  // -----------------------------------------------------------------------
+  // 1. auto cmd = zephir::command::CreateFromArgv(argc, argv);
+  //    1a. auto cwd = zephir::filesystem::GetCurrentWorkingPath();
+  //    1b. auto config = zephir::Config::CreateFromArgv(argc, argv, cwd);
+  // 2. auto retval = cmd.Execute();
+  //    2a. zephir::compiler::CommandName();
+  // 3. return retval;
+  // -----------------------------------------------------------------------
+  // 1..9    Should be invoked from main.cpp
+  // 1a..1z  Should be invoked from command::CreateFromArgv
+  // 2a..2z  Should be invoked from cmd.Execute
+
   return EXIT_SUCCESS;
 }
