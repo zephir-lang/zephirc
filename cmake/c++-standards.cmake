6dc830e0fe8de1cfa89d2a929785def12d8fdf9d
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-20 22:09:50 +0200

    Remove dead cmake code

--- cmake/c++-standards.cmake
+++ cmake/c++-standards.cmake
@@ -19,14 +19,6 @@ macro(cxx_11)
   set(CMAKE_CXX_STANDARD 11)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   set(CMAKE_CXX_EXTENSIONS OFF)
-
-  if(MSVC_VERSION GREATER_EQUAL "1900" AND CMAKE_VERSION LESS 3.10)
-    include(CheckCXXCompilerFlag)
-    check_cxx_compiler_flag("/std:c++11" _cpp_latest_flag_supported)
-    if(_cpp_latest_flag_supported)
-      add_compile_options("/std:c++11")
-    endif()
-  endif()
 endmacro()
 
 # Set the compiler standard to C++14
@@ -36,14 +28,6 @@ macro(cxx_14)
   set(CMAKE_CXX_STANDARD 14)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   set(CMAKE_CXX_EXTENSIONS OFF)
-
-  if(MSVC_VERSION GREATER_EQUAL "1900" AND CMAKE_VERSION LESS 3.10)
-    include(CheckCXXCompilerFlag)
-    check_cxx_compiler_flag("/std:c++14" _cpp_latest_flag_supported)
-    if(_cpp_latest_flag_supported)
-      add_compile_options("/std:c++14")
-    endif()
-  endif()
 endmacro()
 
 # Set the compiler standard to C++17
@@ -53,14 +37,6 @@ macro(cxx_17)
   set(CMAKE_CXX_STANDARD 17)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   set(CMAKE_CXX_EXTENSIONS OFF)
-
-  if(MSVC_VERSION GREATER_EQUAL "1900" AND CMAKE_VERSION LESS 3.10)
-    include(CheckCXXCompilerFlag)
-    check_cxx_compiler_flag("/std:c++17" _cpp_latest_flag_supported)
-    if(_cpp_latest_flag_supported)
-      add_compile_options("/std:c++17")
-    endif()
-  endif()
 endmacro()
 
 # TODO(klay): C++20 is not supportet in currect XCode at this time.
