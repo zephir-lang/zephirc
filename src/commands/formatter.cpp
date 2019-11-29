b60d4631d60a576804f606030260be7643a7b9bc
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-29 19:19:08 +0200

    Better strings initialization

--- src/commands/formatter.cpp
+++ src/commands/formatter.cpp
@@ -15,14 +15,12 @@ zephir::commands::Formatter::Formatter() = default;
 
 std::string zephir::commands::Formatter::make_usage(
     const CLI::App *app, __attribute__((unused)) const std::string name) const {
-  std::stringstream out;
-
-  out << get_label("Usage") << ":" << std::endl;
+  std::string out = get_label("Usage") + ":\n";
 
   if (app->get_parent()) {
-    out << "  " << app->get_name();
+    out += "  " + app->get_name();
   } else {
-    out << "  " << get_label("COMMAND");
+    out += "  " + get_label("COMMAND");
   }
 
   std::vector<std::string> groups = app->get_groups();
@@ -31,7 +29,7 @@ std::string zephir::commands::Formatter::make_usage(
   std::vector<const CLI::Option *> non_positionals = app->get_options(
       [](const CLI::Option *opt) { return opt->nonpositional(); });
   if (!non_positionals.empty()) {
-    out << " [" << get_label("OPTIONS") << "]";
+    out += " [" + get_label("OPTIONS") + "]";
   }
 
   // Print an ARGUMENTS badge if any arguments exist
@@ -39,12 +37,10 @@ std::string zephir::commands::Formatter::make_usage(
   std::vector<const CLI::Option *> positionals = app->get_options(
       [](const CLI::Option *opt) { return opt->get_positional(); });
   if (!app->get_parent() || !positionals.empty()) {
-    out << " [" << get_label("ARGUMENTS") << "]";
+    out += " [" + get_label("ARGUMENTS") + "]";
   }
 
-  out << std::endl;
-
-  return out.str();
+  return out += "\n";
 }
 
 std::string
@@ -57,42 +53,40 @@ zephir::commands::Formatter::make_description(const CLI::App *app) const {
 /____/\___/ .___/_/ /_/_/_/
          /_/)BANNER";
 
-  std::stringstream out;
-  out << "";
+  std::string out;
 
   // Show banner and version only for main app
   if (!app->get_parent()) {
     std::string banner(BANNER);
-    out << banner.replace(0, 1, "") << std::endl << std::endl;
+    out += banner.replace(0, 1, "") + "\n\n";
 
     std::string desc = app->get_description();
-    out << desc << std::endl << std::endl;
+    out += desc + "\n\n";
   }
 
   // TODO(klay): Deal with "min_options" and "max_options".
   // See parent class for more.
 
-  return out.str();
+  return out;
 }
 
 std::string
 zephir::commands::Formatter::make_footer(const CLI::App *app) const {
-  std::stringstream out;
-  out << "";
+  std::string out;
 
   // Show general help usage only for the main app.
   // Any particular command should provide its own concrete help.
   const std::string &footer = app->get_footer();
   if (!app->get_parent() && !footer.empty()) {
-    out << std::endl << footer << std::endl;
+    out += "\n" + footer + "\n";
   } else if (app->get_parent()) {
-    out << std::endl << get_label("Help") << ":" << std::endl;
-    out << "  " << app->get_description() << "." << std::endl;
+    out += "\n" + get_label("Help") + ":\n";
+    out += "  " + app->get_description() + ".\n";
 
     // Any additional command specific help should be
     // declared in the footer.
-    out << "  " << app->get_footer() << std::endl;
+    out += "  " + app->get_footer() + "\n";
   }
 
-  return out.str();
+  return out;
 }
