b8ecb9b2f88aec755f0ed92bd03152d297b11920
Serghei Iakovlev <sadhooklay@gmail.com>
2019-11-30 13:41:19 +0200

    Provide fullclean command

---
+++ src/commands/cmd_clean.hpp
@@ -0,0 +1,38 @@
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+#ifndef ZEPHIR_COMMANDS_CMD_CLEAN_HPP_
+#define ZEPHIR_COMMANDS_CMD_CLEAN_HPP_
+
+#include <string>
+
+#include <CLI/CLI11.hpp>
+
+namespace zephir::commands {
+/**
+ * @brief Cleans any object files created by the extension.
+ */
+class CleanCommand {
+ public:
+  CleanCommand(CLI::App* app, const std::string& group);
+
+ private:
+  /**
+   * Configures the current command.
+   * @param app CLI::App instance
+   * @param group The command group membership
+   */
+  void Configure(CLI::App* app, const std::string& group);
+
+  /**
+   * Executes the current command.
+   */
+  void Execute();
+};
+}  // namespace zephir::commands
+
+#endif  // ZEPHIR_COMMANDS_CMD_CLEAN_HPP_
