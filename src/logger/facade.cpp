6a027fcddab467cde51c81afda01c7c94305b8f3
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-12-02 00:27:02 +0200

    Add base wrapping for common log levels

--- src/logger/facade.cpp
+++ src/logger/facade.cpp
@@ -8,8 +8,7 @@
 #include "facade.hpp"
 
 template <class Type>
-zephir::logger::Facade<Type>::Facade(
-    const std::string& logger_name) {
+zephir::logger::Facade<Type>::Facade(const std::string& logger_name) {
   _logger = spdlog::stdout_color_mt(logger_name);
 }
 
@@ -21,4 +20,32 @@ zephir::logger::Facade<Type>::~Facade() {
 template <class Type>
 void zephir::logger::Facade<Type>::info(const std::string& message) {
   _logger->info(message);
+}
+
+template <class Type>
+void zephir::logger::Facade<Type>::warning(const std::string& message) {
+  _logger->warn(message);
+}
+
+template <class Type>
+void zephir::logger::Facade<Type>::error(const std::string& message) {
+  _logger->error(message);
+}
+
+template <class Type>
+void zephir::logger::Facade<Type>::debug(const std::string& message) {
+  _logger->debug(message);
+}
+
+template <class Type>
+void zephir::logger::Facade<Type>::exception(const std::exception& error,
+                                             const Type& context) {
+  ///
+}
+
+template <class Type>
+void zephir::logger::Facade<Type>::log(const std::string& message, int level,
+                                       const std::string& category,
+                                       const Type& context) {
+  ///
 }
