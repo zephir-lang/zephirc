20ffb21b42863ce5bc70cdfda13ace517f639ecd
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-13 00:59:56 +0200

    Correct code style

--- tests/config/config/stubs.encode.cpp
+++ tests/config/config/stubs.encode.cpp
@@ -6,15 +6,16 @@
 #include "zephir/config/stubs.hpp"
 
 TEST(StubsTest, EncodeClass) {
-  zephir::config::StubsPtr stubs =
-      std::make_shared<zephir::config::Stubs>("ide/%version%/%namespace%", false, "");
+  zephir::config::StubsPtr stubs = std::make_shared<zephir::config::Stubs>(
+      "ide/%version%/%namespace%", false, "");
 
   YAML::Node node;
   node["stubs"] = YAML::convert<zephir::config::StubsPtr>::encode(stubs);
 
   EXPECT_TRUE(node.IsMap());
 
-  EXPECT_EQ("ide/%version%/%namespace%", node["stubs"]["path"].as<std::string>());
+  EXPECT_EQ("ide/%version%/%namespace%",
+            node["stubs"]["path"].as<std::string>());
   EXPECT_FALSE(node["stubs"]["stubs-run-after-generate"].as<bool>());
   EXPECT_EQ("", node["stubs"]["banner"].as<std::string>());
 }
