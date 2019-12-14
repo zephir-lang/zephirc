82f69956c190e02340dd0610969fea18a291f940
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-12-14 23:59:12 +0200

    Add initial test suite for logger

---
+++ tests/logger/logger.base.cpp
@@ -0,0 +1,21 @@
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+#include <gtest/gtest.h>
+
+#include "logger/facade.hpp"
+
+class LoggerBaseTest : public ::testing::Test {
+ protected:
+  LoggerBaseTest() = default;
+};
+
+TEST_F(LoggerBaseTest, SimpleLogInfo) {
+  auto test_logger = new zephir::logger::Facade();
+
+  test_logger->info("zephir");
+}
