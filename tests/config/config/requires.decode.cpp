70fcb6bcd77c4c416ebace05c9869ca8ec2d7174
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-15 10:51:57 +0200

    WIP. Cleaned up requires config section

--- tests/config/config/requires.decode.cpp
+++ tests/config/config/requires.decode.cpp
@@ -11,23 +11,20 @@
 #include <zephir/config/requires.hpp>
 
 TEST(RequiresTest, DecodeString) {
-  auto node = YAML::Load(R"(
-requires: {
-  extensions: [PDO, SPL, standard, hash, json]
-})");
+  auto yaml = YAML::Load(R"(
+{ extensions: [PDO, SPL, standard, hash, json] })");
 
   zephir::config::Requires expected({"PDO", "SPL", "standard", "hash", "json"});
   auto actual = std::make_shared<zephir::config::Requires>();
 
-  EXPECT_TRUE(YAML::convert<zephir::config::RequiresPtr>::decode(
-      node["requires"], actual));
+  EXPECT_TRUE(YAML::convert<zephir::config::RequiresPtr>::decode(yaml, actual));
   EXPECT_EQ(*actual, expected);
 }
 
 TEST(RequiresTest, DecodeInvalid) {
-  auto node = YAML::Load("foo: bar");
+  auto yaml = YAML::Load("foo: bar");
   auto actual = std::make_shared<zephir::config::Requires>();
 
   EXPECT_FALSE(
-      YAML::convert<zephir::config::RequiresPtr>::decode(node["foo"], actual));
+      YAML::convert<zephir::config::RequiresPtr>::decode(yaml["foo"], actual));
 }
