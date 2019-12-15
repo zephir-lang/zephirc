cfca84779750251686cb5c40300a7e9dc0fa4216
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-15 14:59:24 +0200

    WIP. Amended config tests

--- tests/config/config.encode.cpp
+++ tests/config/config.encode.cpp
@@ -21,7 +21,6 @@ TEST(ConfigTest, EncodeClass) {
   }
 
   auto file = tests_root + "/fixtures/legacy.yml";
-
   input_t argv({});
 
   auto config = zephir::Config::CreateFromArgv(argv, file);
@@ -45,18 +44,18 @@ TEST(ConfigTest, EncodeClass) {
 
   EXPECT_TRUE(yaml["stubs"].IsMap());
   auto stubs = yaml["stubs"];
+
   EXPECT_EQ("ide/%version%/%namespace%", stubs["path"].as<std::string>());
   EXPECT_FALSE(stubs["stubs-run-after-generate"].as<bool>());
   EXPECT_EQ("", stubs["banner"].as<std::string>());
-
   EXPECT_TRUE(yaml["api"].IsMap());
   EXPECT_EQ("doc/%version%", yaml["api"]["path"].as<std::string>());
   EXPECT_EQ("zephir", yaml["api"]["theme"]["name"].as<std::string>());
 
   EXPECT_TRUE(yaml["api"]["theme"].IsMap());
   EXPECT_TRUE(yaml["api"]["theme"]["options"].IsMap());
-
   auto options = yaml["api"]["theme"]["options"];
+
   EXPECT_EQ("", options["github"].as<std::string>());
   EXPECT_EQ("", options["analytics"].as<std::string>());
   EXPECT_EQ("#3E6496", options["main_color"].as<std::string>());
@@ -64,6 +63,48 @@ TEST(ConfigTest, EncodeClass) {
   EXPECT_EQ("#5F9AE7", options["link_hover_color"].as<std::string>());
 
   EXPECT_TRUE(yaml["warnings"].IsMap());
+  auto warn = yaml["warnings"];
+
+  EXPECT_TRUE(warn["unused-variable"].as<bool>());
+  EXPECT_FALSE(warn["unused-variable-external"].as<bool>());
+  EXPECT_TRUE(warn["possible-wrong-parameter"].as<bool>());
+  EXPECT_FALSE(warn["possible-wrong-parameter-undefined"].as<bool>());
+  EXPECT_TRUE(warn["nonexistent-function"].as<bool>());
+  EXPECT_TRUE(warn["nonexistent-class"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-isset"].as<bool>());
+  EXPECT_TRUE(warn["non-array-update"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-objectupdate"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-fetch"].as<bool>());
+  EXPECT_TRUE(warn["invalid-array-index"].as<bool>());
+  EXPECT_TRUE(warn["non-array-append"].as<bool>());
+  EXPECT_TRUE(warn["invalid-return-type"].as<bool>());
+  EXPECT_TRUE(warn["unreachable-code"].as<bool>());
+  EXPECT_TRUE(warn["nonexistent-constant"].as<bool>());
+  EXPECT_TRUE(warn["not-supported-magic-constant"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-decrement"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-increment"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-clone"].as<bool>());
+  EXPECT_TRUE(warn["non-valid-new"].as<bool>());
+  EXPECT_TRUE(warn["non-array-access"].as<bool>());
+  EXPECT_TRUE(warn["invalid-reference"].as<bool>());
+  EXPECT_TRUE(warn["invalid-typeof-comparison"].as<bool>());
+  EXPECT_TRUE(warn["conditional-initialization"].as<bool>());
+
   EXPECT_TRUE(yaml["optimizations"].IsMap());
+  auto opts = yaml["optimizations"];
+
+  EXPECT_TRUE(opts["static-type-inference"].as<bool>());
+  EXPECT_TRUE(opts["static-type-inference-second-pass"].as<bool>());
+  EXPECT_TRUE(opts["local-context-pass"].as<bool>());
+  EXPECT_TRUE(opts["constant-folding"].as<bool>());
+  EXPECT_TRUE(opts["static-constant-class-folding"].as<bool>());
+  EXPECT_TRUE(opts["call-gatherer-pass"].as<bool>());
+  EXPECT_FALSE(opts["check-invalid-reads"].as<bool>());
+  EXPECT_FALSE(opts["internal-call-transformation"].as<bool>());
+
   EXPECT_TRUE(yaml["extra"].IsMap());
+  auto extra = yaml["extra"];
+
+  EXPECT_EQ("spaces", extra["indent"].as<std::string>());
+  EXPECT_FALSE(extra["export-classes"].as<bool>());
 }
