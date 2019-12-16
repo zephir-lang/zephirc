8bb96c3119dd8ac15b9928a1c2a3ffd862b7d6bd
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-16 20:01:15 +0200

    WIP. Initial command interface

---
+++ include/zephir/cli/commands/command.hpp
@@ -0,0 +1,15 @@
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+#ifndef ZEPHIR_CLI_COMMANDS_COMMAND_HPP_
+#define ZEPHIR_CLI_COMMANDS_COMMAND_HPP_
+
+namespace zephir::cli::commands {
+class Command {};
+}  // namespace zephir::cli::commands
+
+#endif  // ZEPHIR_CLI_COMMANDS_COMMAND_HPP_
