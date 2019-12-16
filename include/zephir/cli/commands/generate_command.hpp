e7347afed89feffd29a5f07d74cdbe9961c968a7
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-17 01:26:01 +0200

    WIP. Add GenerateCommand II

---
+++ include/zephir/cli/commands/generate_command.hpp
@@ -0,0 +1,31 @@
+// This file is part of the Zephir.
+//
+// (c) Zephir Team <team@zephir-lang.com>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+#ifndef ZEPHIR_CLI_COMMANDS_GENERATE_COMMAND_HPP_
+#define ZEPHIR_CLI_COMMANDS_GENERATE_COMMAND_HPP_
+
+#include <zephir/cli/commands/command.hpp>
+
+namespace zephir::cli::commands {
+struct GenerateOptions {
+  std::string backend;
+};
+
+using GenerateOptionsPtr = std::unique_ptr<GenerateOptions>;
+
+class GenerateCommand : public Command {
+ public:
+  explicit GenerateCommand(std::string name);
+  void Setup(std::shared_ptr<CLI::App> app) override;
+  void Execute() override;
+
+ private:
+  GenerateOptionsPtr options_;
+};
+}  // namespace zephir::cli::commands
+
+#endif  // ZEPHIR_CLI_COMMANDS_GENERATE_COMMAND_HPP_
