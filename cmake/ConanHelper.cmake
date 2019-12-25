4b7a4178e731182683efb0a7e0117dfc7d89930e
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-25 21:20:52 +0200

    Added missed CMake definitions, fix CS

---
+++ cmake/ConanHelper.cmake
@@ -0,0 +1,9 @@
+# This file is part of the Zephir.
+#
+# (c) Phalcon Team <team@zephir-lang.com>
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
+set(CONAN_CMAKE_CXX_STANDARD ${CMAKE_CXX_STANDARD})
+set(CONAN_CMAKE_CXX_EXTENSIONS ${CMAKE_CXX_EXTENSIONS})
