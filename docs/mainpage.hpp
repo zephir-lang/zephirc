dd3af54a2d1af876e7ab36083f1d1ce6ea2a87ba
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-28 12:56:05 +0200

    Correct CS

--- docs/mainpage.hpp
+++ docs/mainpage.hpp
@@ -2,7 +2,7 @@
 /// \mainpage
 ///
 /// \section welcome Welcome
-/// This is the Doxygen API documentation for Zephir compiler. The purpose of this
-/// documentation is to explain how Zephir is built internally. If you're interested
-/// in helping to improve Zephir, this is the information you need.
+/// This is the Doxygen API documentation for Zephir compiler. The purpose of
+/// this documentation is to explain how Zephir is built internally. If you're
+/// interested in helping to improve Zephir, this is the information you need.
 ////////////////////////////////////////////////////////////
