d702ae2342cfa17f80b81915b847788ffe16f59d
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-04 12:12:58 +0200

    Rework version variables

--- cmake/ProjectInfo.cmake
+++ cmake/ProjectInfo.cmake
@@ -5,32 +5,42 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-set(_re "#define[ \t]+ZEPHIR_VERSION_(MAJOR|MINOR|PATCH)")
+set(VERSION_RE "#define[ \t]+ZEPHIR_VERSION_(MAJOR|MINOR|PATCH|TWEAK|EXTRA)")
 
-file(STRINGS "${zephir_SOURCE_DIR}/include/zephir/version.hpp" _parts
-     REGEX ${_re})
+file(STRINGS "${zephir_SOURCE_DIR}/include/zephir/version.hpp" VERSION_PARTS
+     REGEX ${VERSION_RE})
 
-foreach(_part ${_parts})
-  if(_part MATCHES "${_re}[ \t]+([^ ]+)$")
-    set(ZEPHIR_VERSION_${CMAKE_MATCH_1}
+foreach(PART ${VERSION_PARTS})
+  if(PART MATCHES "${VERSION_RE}[ \t]+([^ ]+)$")
+    set(PROJECT_VERSION_${CMAKE_MATCH_1}
         "${CMAKE_MATCH_2}"
         CACHE INTERNAL "")
   endif()
 endforeach()
 
-set(PROJECT_VERSION
-    "${ZEPHIR_VERSION_MAJOR}.${ZEPHIR_VERSION_MINOR}.${ZEPHIR_VERSION_PATCH}")
-set(PROJECT_VERSION_MAJOR ${ZEPHIR_VERSION_MAJOR})
-set(PROJECT_VERSION_MINOR ${ZEPHIR_VERSION_MINOR})
-set(PROJECT_VERSION_PATCH ${ZEPHIR_VERSION_PATCH})
-set(PROJECT_VERSION_TWEAK 0) # Not used for now
+set(PROJECT_VERSION_WITHOUT_TWEAK ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH})
+
+if(PROJECT_VERSION_TWEAK)
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK}.${PROJECT_VERSION_TWEAK})
+else()
+  set(PROJECT_VERSION ${PROJECT_VERSION_WITHOUT_TWEAK})
+endif()
 
 set(BUILD_SUFFIX)
+if(PROJECT_VERSION_EXTRA)
+  set(BUILD_SUFFIX "-${PROJECT_VERSION_EXTRA}")
+endif()
 
-set(ZEPHIR_DISPLAY_NAME Zephir)
+set(PROJECT_DISPLAY_NAME Zephir)
 
 string(TIMESTAMP CURRENT_YEAR %Y)
 set(ZEPHIR_TEAM "Phalcon Team")
 set(ZEPHIR_COPYRIGHT "© 2013-${CURRENT_YEAR} ${ZEPHIR_TEAM}")
 
 set(PROJECT_DESCRIPTION "Yet another implementation of the Zephir compiler")
+
+unset(PART)
+unset(VERSION_RE)
+unset(VERSION_PARTS)
+unset(CURRENT_YEAR)
+unset(PROJECT_VERSION_WITHOUT_TWEAK)
