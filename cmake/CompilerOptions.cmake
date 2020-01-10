bbae6eef99f78300c73807de3450e90ace1ff92f
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-10 13:51:58 +0200

    Remove libc++ support for Linux
    libc++'s C++17 implementation is not yet complete

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -10,17 +10,6 @@ include(CMakeDependentOption)
 
 option(WARNINGS_AS_ERRORS "Turn all build warnings into errors")
 
-list(APPEND force-libcxx "CMAKE_CXX_COMPILER_ID STREQUAL \"Clang\"")
-list(APPEND force-libcxx "CMAKE_SYSTEM_NAME STREQUAL \"Linux\"")
-list(APPEND force-libcxx "CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME")
-
-# This macro provides ZEPHIR_FORCE_LIBCXX option to the user only if a set of
-# `force-libcxx' conditions are true.
-cmake_dependent_option(
-  ZEPHIR_FORCE_LIBCXX
-  "Force clang to use libc++ instead of libstdc++ (Linux only)" OFF
-  "${force-libcxx}" OFF)
-
 # Special target that adds warnings. Is not exported.
 add_library(zephir_warnings INTERFACE)
 
@@ -35,13 +24,6 @@ endif()
 
 target_compile_options(
   zephir_warnings
-  INTERFACE $<$<BOOL:${ZEPHIR_FORCE_LIBCXX}>:-stdlib=libc++>
-            $<$<CXX_COMPILER_ID:MSVC>:/W4
-            $<$<BOOL:${WARNINGS_AS_ERRORS}>:/WX>>
+  INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/W4 $<$<BOOL:${WARNINGS_AS_ERRORS}>:/WX>>
             $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:${unix-warnings}
             $<$<BOOL:${WARNINGS_AS_ERRORS}>:-Werror>>)
-
-if(NOT CMAKE_VERSION VERSION_LESS 3.13)
-  target_link_options(zephir_warnings INTERFACE
-                      $<$<BOOL:${ZEPHIR_FORCE_LIBCXX}>:-stdlib=libc++>)
-endif()
